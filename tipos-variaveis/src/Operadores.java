0a19ae3a9940c4227bd32e5b408e2fcb3fe4636e
Bruno Mota <bmotadev@gmail.com>
2023-02-10 20:11:11 -0300

    Operadores - Concatenação

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,28 @@
+public class Operadores {
+    public static void main(String[] args) {
+        //classe Operadores.java
+                    
+            //qual o resultado das expressoes abaixo?
+            String concatenacao ="?"; 
+
+            concatenacao = 1+1+1+"1";
+
+            System.out.println(concatenacao);
+
+            concatenacao = 1+"1"+1+1;
+
+            System.out.println(concatenacao);
+
+            concatenacao = 1+"1"+1+"1";
+
+            System.out.println(concatenacao);
+
+            concatenacao = "1"+1+1+1;
+
+            System.out.println(concatenacao);
+
+            concatenacao = "1"+(1+1+1);
+
+            System.out.println(concatenacao);
+    }
+}
