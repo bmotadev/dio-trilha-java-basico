1c8c276ce0fdf06bc4bd06dd31249a2343acd758
Bruno Mota <bmotadev@gmail.com>
2023-02-10 17:12:11 -0300

    Tipos e Variaveis - Constantes e Variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -2,12 +2,21 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         double salarioMinimo = 2500.33;
-        System.out.println("Hello, World!");
 
         // TiposEVariaveis.java
 
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short)numeroNormal; //casting
+
+        int numero = 1;
+        numero = 2;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14; // final é constante
+        VALOR_DE_PI = 10.75;
+
+        
     }
 }
