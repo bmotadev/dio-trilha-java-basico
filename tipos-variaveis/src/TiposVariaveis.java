7006a816e43a7567ae08685b4999a5509b47e023
Bruno Mota <bmotadev@gmail.com>
2023-02-10 16:59:50 -0300

    Tipos e Variaveis - Parte 3 - Casting - Tipos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
+
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500.33;
         System.out.println("Hello, World!");
+
+        // TiposEVariaveis.java
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroNormal; //casting
     }
 }
